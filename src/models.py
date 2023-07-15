8b5f63662ed5fe87232b3c806c4338607abba8be
fpineiro3567 <128890463+fpineiro3567@users.noreply.github.com>
2023-07-15 14:55:49 +0000

    updated and completed star wars data model

--- src/models.py
+++ src/models.py
@@ -9,67 +9,81 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    password = Column(String(250))
+    # favorites = relationship('Favorites')
+    favorites = Column(Integer, ForeignKey('favorites.id'))
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    characters = relationship('Characters')
+    planets = relationship('Planets')
+    vehicles = relationship('Vehicles')
+    users = relationship('User')
+    # characters = Column(Integer, ForeignKey('characters.id'))
+    # planets = Column(Integer, ForeignKey('planets.id'))
+    # vehicles = Column(Integer, ForeignKey('vehicles.id'))
+    # users = Column(Integer, ForeignKey('user.id'))
+
+   
+
+
 
 class Characters(Base):
     __tablename__ = 'characters'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    height = Column(Integer, primary_key=True)
-    mass = Column(Integer, primary_key=True)
+    height = Column(Integer)
+    mass = Column(Integer)
     hair_color = Column(String(250))
     skin_color = Column(String(250))
     eye_color = Column(String(250))
     birthyear = Column(String(250))
     gender = Column(String(250))
+    # favorites = relationship('Favorites')
+    favorites = Column(Integer, ForeignKey('favorites.id'))
 
 class Vehicles(Base):
     __tablename__ = 'vehicles'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    model = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    model = Column(Integer)
     starship_class = Column(String(250))
     manufacturer = Column(String(250))
     cost_in_credits = Column(String(250))
-    length = Column(Integer, primary_key=True)
+    length = Column(Integer)
     crew = Column(String(250))
     passengers = Column(String(250))
     hyperdrive_rating = Column(String(250))
     mglt = Column(String(250))
-    cargo_capacity = Column(Integer, primary_key=True)
+    cargo_capacity = Column(Integer)
     consumables = Column(String(250))
-    pilot_id = Column(Integer, ForeignKey('characters.id'))
-    pilot = relationship("Characters")
     created = Column(String(250))
     edited = Column(String(250))
     name = Column(String(250))
-
+    # favorites = relationship('Favorites')
+    favorites = Column(Integer, ForeignKey('favorites.id'))
+    
 
 class Planets(Base):
     __tablename__ = 'planets'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    diameter = Column(Integer, primary_key=True)
-    rotation_period = Column(Integer, primary_key=True)
-    orbital_period = Column(Integer, primary_key=True)
-    gravity = Column(Integer, primary_key=True)
-    population = Column(Integer, primary_key=True)
-    climate = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(Integer)
+    population = Column(Integer)
+    climate = Column(Integer)
     terrain = Column(String(250))
     surface_water = Column(String(250), nullable=True)
     created = Column(String(250))
     edited = Column(String(250))
     name = Column(String(250))
+    # favorites = relationship('Favorites')
+    favorites = Column(Integer, ForeignKey('favorites.id'))
 
-   
-   
-
-    def to_dict(self):
-        return {}
+def to_dict(self):
+    return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
